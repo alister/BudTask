49ad33d58b74859c8181fe5f1d2bc93351f2f932
Alister Bulman <abulman@gmail.com>
2019-05-17 14:06:30 +0100

    Extract the mock/history client generation

--- tests/Alister/Bud/DeathStarApiClientTest.php
+++ tests/Alister/Bud/DeathStarApiClientTest.php
@@ -17,36 +17,27 @@ class DeathStarApiClientTest extends TestCase
     const CLIENT_ID = 'R2D2';
     const EXPECTED_API_URI_TOKEN = 'https://death.star.api/token';
 
+    private $container = [];
+
     public function testGetTokenSuccessfullyMocked()
     {
         // randomise what we expect to get back to assure ourselves it's being passed back exactly.
         $randomisedAcessToken = base64_encode(random_bytes(16));
-        $goodResponse = [
+        $mockedResponsed = [
             'access_token' => $randomisedAcessToken,
             'expires_in' => 99999999999,
             'token_type' => 'Bearer',
             'scope' => 'TheForce'
         ];
-        $mock = new MockHandler([
-            new Response(200, $goodResponse),
-        ]);
+        $client = $this->createMockDeathStarApiClient($mockedResponsed);
 
-        $container = [];
-        $history = Middleware::history($container);
-        $stack = HandlerStack::create($mock);
-        $stack->push($history);
-
-        // normally setup by config, and injected by DI
-        $guzzleClient = new Client(['handler' => $stack, 'base_uri' => DeathStarApiClient::API_BASE_URI]);
-
-        $client = new DeathStarApiClient($guzzleClient);
         $result = $client->getToken(self::CLIENT_ID, self::CLIENT_SECRET);
 
         // the headers are returned as arrays, so check for the '[ content ]'
         $this->assertSame($result->getHeader('access_token'), [$randomisedAcessToken]);
 
-        $this->assertCount(1, $container);
-        $guzzleTransaction = $container[0];
+        $this->assertCount(1, $this->container);
+        $guzzleTransaction = $this->container[0];
 
         /** @var GuzzleHttp\Psr7\Request $request */
         $request = $guzzleTransaction['request'];
@@ -56,5 +47,24 @@ class DeathStarApiClientTest extends TestCase
         $this->assertSame('POST', $request->getMethod());
         $this->assertSame(self::EXPECTED_API_URI_TOKEN, (string)$request->getUri());
         $this->assertSame(200, $response->getStatusCode());
+    }
+
+    /**
+     * @param array $mockedResponsed
+     */
+    private function createMockDeathStarApiClient(array $mockedResponsed): DeathStarApiClient
+    {
+        $mock = new MockHandler([
+            new Response(200, $mockedResponsed),
+        ]);
+
+        $history = Middleware::history($this->container);
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+
+        // normally setup by config, and injected by DI
+        $guzzleClient = new Client(['handler' => $stack, 'base_uri' => DeathStarApiClient::API_BASE_URI]);
+
+        return new DeathStarApiClient($guzzleClient);
     }
 }
